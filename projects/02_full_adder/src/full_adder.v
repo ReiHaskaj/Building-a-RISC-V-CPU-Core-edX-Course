35cd5c1c1a04076e1f7393ed21cb6aff90a746e0
ReiHaskaj <reihaskaj98@gmail.com>
2025-12-15 14:07:39 +0100

    Remove redundant comments for readability.
    No functional changes.

--- projects/02_full_adder/src/full_adder.v
+++ projects/02_full_adder/src/full_adder.v
@@ -1,25 +1,23 @@
 \m5_TLV_version 1d: tl-x.org
 \m5
-   
-   // ============================================
-   // Welcome, new visitors! Try the "Learn" menu.
-   // ============================================
-   
+
    //use(m5-1.0)   /// uncomment to use M5 macro library.
+
 \SV
    // Macro providing required top-level module definition, random
    // stimulus support, and Verilator config.
-   m5_makerchip_module   // (Expanded in Nav-TLV pane.)
+   m5_makerchip_module
+
 \TLV
    $reset = *reset;
    
    $out1 = $val1 ^ $val2;
-   $out2 = $val1 && $val2;
+   $out2 = $val1 && $val2;  // Carry from the first half_adder.
    
-   $sum = $out1 ^ $carryin;
-   $out3 = $out1 && $carryin;
+   $sum = $out1 ^ $carryin;  // Sum result.
+   $out3 = $out1 && $carryin;  // Carry from the second half_adder.
    
-   $carryout = $out3 || $out2;
+   $carryout = $out3 || $out2;  // Carry out result.
    
    // Assert these to end simulation (before the cycle limit).
    *passed = *cyc_cnt > 40;
